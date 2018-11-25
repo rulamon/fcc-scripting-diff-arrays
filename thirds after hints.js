e356719e54f0e46d07b7be317aef7ee90c9183e5
rulamon <ruben.lamon@gmail.com>
2018-11-25 20:24:18 +0100

    arrow function for third

--- thirds after hints.js
+++ thirds after hints.js
@@ -1,3 +1,2 @@
-const diffArray = (arr1, arr2) => arr1.filter(x => !arr2.includes(x)).concat(arr2.filter(x => !arr1.includes(x)))
-
+const diffArray = (arr1, arr2) => arr2.concat(arr1).filter(x => !arr1.includes(x) || !arr2.includes(x));
 console.log(diffArray([1, 2, 3, 5,], [1, 2, 3, 4, 5,]));
